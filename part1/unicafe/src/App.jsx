5b8875ef78f1be00b06b021d0add64dbf3496144
jamilnim <jamilnimbook2@gmail.com>
2026-03-09 00:38:09 +0200

    part1 step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,19 +10,44 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return (
+  const total = good + neutral + bad;
+  const average = total===0?0:(good - bad) / total;
+  const percentagePosgtive = total===0?0:(good/ total)*100;
 
+  return (
     <div>
       <h1>Please give your feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <Button handleClick={() => setGood(0) & setNeutral(0) & setBad(0)} text="Reset" />
-      
-    <div><h2>Good: {good}</h2></div>
-    <div><h2>Neutral: {neutral}</h2></div>
-    <div><h2>Bad: {bad}</h2></div>
+      <Button
+        handleClick={() => setGood(0) & setNeutral(0) & setBad(0)}
+        text="Reset"
+      />
 
+      <div>
+        <h2>Good: {good}</h2>
+      </div>
+      <div>
+        <h2>Neutral: {neutral}</h2>
+      </div>
+      <div>
+        <h2>Bad: {bad}</h2>
+      </div>
+      <div>
+        <h2>Total: {total}</h2>
+      </div>
+      <div>
+        <h2>
+          Average:
+          {total===0? "No Feedback" :average.toFixed(2)}
+        </h2>
+      </div>
+
+      <div>
+        <h2>Posivite percentage: {total===0? "0": percentagePosgtive.toFixed(2)}%</h2>
+      </div>
+     
     </div>
   );
 };
